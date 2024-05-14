9279394d14fe0aa88fe08a11d5a460943e081169
ponypown <ponypown@gmail.com>
2024-05-14 12:16:46 -0700

    change model name

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from transformers import AutoModelForSeq2SeqLM
 
 if __name__ == '__main__':
     # gets the model
-    tokenizer = AutoTokenizer.from_pretrained("my_awesome_opus_books_model")
+    tokenizer = AutoTokenizer.from_pretrained("t5_opus_books_de_en_model")
 
     # old way, goes through hub
-    # translator = pipeline("translation_de_to_en", model='my_awesome_opus_books_model') # before renaming
     # translator = pipeline("translation_de_to_en", model='t5_opus_books_de_en_model') # after rename
     
     # prefix for translation
@@ -21,7 +20,7 @@ if __name__ == '__main__':
     inputs = tokenizer(text, return_tensors="pt").input_ids
 
     # generates the outputs using the model
-    model = AutoModelForSeq2SeqLM.from_pretrained("my_awesome_opus_books_model")
+    model = AutoModelForSeq2SeqLM.from_pretrained("t5_opus_books_de_en_model")
     outputs = model.generate(inputs, max_new_tokens=40, do_sample=True, top_k=30, top_p=0.95)
 
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
